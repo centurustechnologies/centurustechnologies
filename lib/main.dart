4eb9b03c47efc304c926edc74021f1ff3c804f29
centurustechnologies <centurustechnologies@gmail.com>
2022-11-24 15:01:38 +0530

    "experience page"

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:centurus_web_app/view/contact_us_screen.dart';
 import 'package:centurus_web_app/view/dashboard.dart';
 import 'package:centurus_web_app/view/helpers/app_constants.dart';
 import 'package:centurus_web_app/view/ourservices/helper/ourservices.dart';
+import 'package:centurus_web_app/view/viewus/helper/viewus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Centurus Technologies Pvt. Ltd.',
       debugShowCheckedModeBanner: false,
-      home:ourServices()
+      home:viewUs()
     );
   }
 }
---
+++ lib/view/viewus/helper/viewus.dart
@@ -0,0 +1,72 @@
+import 'dart:developer';
+
+import 'package:centurus_web_app/view/blog/helper/headertitle.dart';
+import 'package:centurus_web_app/view/header_widget.dart';
+import 'package:centurus_web_app/view/helpers/clientsay.dart';
+import 'package:centurus_web_app/view/helpers/contactus.dart';
+import 'package:centurus_web_app/view/helpers/footer.dart';
+import 'package:centurus_web_app/view/helpers/services.dart';
+import 'package:centurus_web_app/view/navBar/desktop_navBar.dart';
+import 'package:centurus_web_app/view/navBar/mobile_navbar.dart';
+import 'package:centurus_web_app/view/helpers/app_constants.dart';
+import 'package:centurus_web_app/view/helpers/banners.dart';
+import 'package:centurus_web_app/view/viewus/helper/experience.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+
+
+
+
+
+class viewUs extends StatefulWidget {
+  const viewUs({Key? key}) : super(key: key);
+
+  @override
+  State<viewUs> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<viewUs>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      key: scaffoldKey,
+      endDrawer: const Drawer(
+        child: DrawerWidget(),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              HeaderWidget(width: width, scaffoldKey: scaffoldKey),
+              headerTitle(context),
+              Experience(context),
+              //servicesDesktopMethod(context),
+              //clientSayDesktopMethod(context),
+              //ContactUsDesktopMethod(context),
+              Footer(context,whiteColor,mainColor),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
